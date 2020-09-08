b8a6948346c0a7404e26b05e5be89f193f13a132
김소영 <soyeong.kim.95@gmail.com>
2020-09-09 00:06:30 +0900

    0909 update

--- 01.SWEA/D4/3752.가능한 시험 점수.py
+++ 01.SWEA/D4/3752.가능한 시험 점수.py
@@ -1,7 +1,23 @@
 for tc in range(1, int(input()) + 1):
     N = int(input())
     scores = list(map(int, input().split()))
+    M = 100 ** 2
+    check = [0] * M
+    check[0] = 1
 
-    # 부분집합
+    for score in scores:
+        numbers = []
+
+        for num in range(M):
+            if check[num]:
+                numbers.append(num)
+
+        for number in numbers:
+            if check[number] and not check[number + score]:
+                check[number + score] = 1
+
+    print('#{} {}'.format(tc, check.count(1)))
 
 
+# dfs로 풀면 시간초과남
+# check배열에 표시해 나가면서 반복으로 풀어야함.
